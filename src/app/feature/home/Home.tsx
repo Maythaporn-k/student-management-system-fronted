b74eed5e14e6150e4de284233ad614609f4cd7ef
Maythaporn <maythaporn.k@arise.tech>
2025-01-23 10:39:22 +0700

    fix merge conflict

--- src/app/feature/home/Home.tsx
+++ src/app/feature/home/Home.tsx
@@ -3,11 +3,7 @@ import StudentList from "./StudentData";
 
 const HomePage: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <div className="bg-white p-6 rounded-lg shadow-md ">
-=======
     <div className="bg-white p-6 rounded-lg shadow-md h-max">
->>>>>>> code
       <h2 className="text-3xl font-semibold text-gray-800 mt-4 mb-4 text-center">
         Welcome to the Student Management Dashboard
       </h2>
