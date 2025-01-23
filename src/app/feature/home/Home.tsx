293b0e868abf72cf56cce1c489d4749a6cd9093f
Maythaporn <maythaporn.k@arise.tech>
2025-01-23 10:18:13 +0700

    fix olayflow

--- src/app/feature/home/Home.tsx
+++ src/app/feature/home/Home.tsx
@@ -3,8 +3,13 @@ import StudentList from "./StudentData";
 
 const HomePage: React.FC = () => {
   return (
+<<<<<<< Updated upstream
     <div className="bg-white p-6 rounded-lg shadow-md ">
       <h2 className="text-3xl font-semibold text-gray-800 mt-4 text-center h-fit">
+=======
+    <div className="bg-white p-6 rounded-lg shadow-md h-max">
+      <h2 className="text-3xl font-semibold text-gray-800 mt-4 mb-4 text-center">
+>>>>>>> Stashed changes
         Welcome to the Student Management Dashboard
       </h2>
 
